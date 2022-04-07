309aca22b99308635a7874148163a8962ed0d292
abam112 <abam.akbar30@gmail.com>
2022-04-07 09:56:38 +0700

    first commit

---
+++ app/Views/layouts/header.php
@@ -0,0 +1,13 @@
+<!-- As a link -->
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Mochammad Akbar Fajarsyah</a>
+  </div>
+</nav>
+
+<!-- As a heading -->
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1"> <p><em>4ITE1 EIS(Developing Enterprises Information System using Framework)</em></p></span>
+  </div>
+</nav>
---
+++ app/Views/layouts/navbar.php
@@ -0,0 +1,7 @@
+
+<header>
+    <nav class="navbar navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="index.php">Mochammad Akbar Fajarsyah</a>
+        
+    </nav>
+</header>
